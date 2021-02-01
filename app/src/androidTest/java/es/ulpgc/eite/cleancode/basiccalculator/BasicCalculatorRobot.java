48802da51e56c7b7c9d205245b4630fc336b431a
Luis Hernandez <lhdez@iuma.ulpgc.es>
2021-02-01 12:23:13 +0000

    cambios menores

--- app/src/androidTest/java/es/ulpgc/eite/cleancode/basiccalculator/BasicCalculatorRobot.java
+++ app/src/androidTest/java/es/ulpgc/eite/cleancode/basiccalculator/BasicCalculatorRobot.java
@@ -11,8 +11,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class BasicCalculatorRobot {
 
-
-
     public void calculatorShowsInFirstOperandTheValueP1(String op1) {
         onView(withId(R.id.op1Input)).check(matches(withText(op1)));
     }
@@ -36,19 +34,14 @@ public class BasicCalculatorRobot {
     }
 
     public void userClicksOnTheOperationP1(String op) {
-
         if(op.equals("+")) {
             onView(withId(R.id.addButton)).perform(click());
-
         } else if(op.equals("-")) {
             onView(withId(R.id.subButton)).perform(click());
-
         } else if(op.equals("x")) {
             onView(withId(R.id.mulButton)).perform(click());
-
         } else if(op.equals("/")) {
             onView(withId(R.id.divButton)).perform(click());
-
         } else {
             onView(withId(R.id.calButton)).perform(click());
         }
@@ -57,5 +50,4 @@ public class BasicCalculatorRobot {
     public void calculatorShowsInResultTheValueP1(String res) {
         onView(withId(R.id.resText)).check(matches(withText(res)));
     }
-
 }
